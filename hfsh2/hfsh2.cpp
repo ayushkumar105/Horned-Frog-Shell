53302e5bbb5b831b9188610ac3ce3ce174ddc93f
ayushkumar105 <ayush.kumar@tcu.edu>
2023-02-10 20:17:36 -0600

    First Final Commit

--- hfsh2/hfsh2.cpp
+++ hfsh2/hfsh2.cpp
@@ -49,11 +49,7 @@ extern "C" {
 
 //*********************************************************
 //
-// Namespace
-//
-// If you are new to C++, uncommment the `using namespace std` line.
-// It will make your life a little easier.  However, all true
-// C++ developers will leave this commented.  ;^)
+// Structs and global variables.
 //
 //*********************************************************
 struct command{
@@ -71,9 +67,12 @@ vector<string> built_in = {"cd", "path", "exit"};
 //*********************************************************
 int shell(Command& comm);
 bool checkBuiltIn(const Command& command);
-string parseInput(const string& input);
-void executeCommand(const Command& command);
+void executeBuiltCommand(const Command& command);
 int executeCommands(vector<Command>& commands);
+int batchFile(char* filename);
+string isPathValid(const vector<string>& paths, const Command& comm);
+int checkForRedirect(Command& comm);
+int parseCommand(const Command& comm, char* args[], string& commPath, string& path);
 
 
 //*********************************************************
@@ -81,9 +80,20 @@ int executeCommands(vector<Command>& commands);
 // Function Definitions
 //
 //*********************************************************
+
+
+/****************************************************************************************************************
+*   Executes all the commands by creating a child process for each command that then executes the shell function.
+*   Then it waits for each of the child processes created by it. 
+*   Or if it is a built-in command then it calls the executeCommand function to handle those commands.
+*****************************************************************************************************************/
 int executeCommands(vector<Command>& commands)
 {
+    //Create an array of PID for each command.
     pid_t pid[commands.size()];
+    int childNum = 0;
+
+    //Iterate through the command vector to either create a child to run a command or just run a built-in comamnd. 
     for(int i =0; i< commands.size(); ++i)
     {
         if(!(checkBuiltIn(commands[i])))
@@ -95,17 +105,14 @@ int executeCommands(vector<Command>& commands)
                 shell(commands[i]);
                 exit(0);
             }
-            // if(pid != 0)
-            // {
-            //     waitpid(pid, NULL, 0);
-            // }
         }
         else
         {
-            executeCommand(commands[i]);
+            ++childNum;
+            executeBuiltCommand(commands[i]);
         }
     }
-    for(int i = 0; i< commands.size(); ++i)
+    for(int i = 0; i< commands.size() - childNum; ++i)
     { 
         waitpid(pid[i], NULL, 0);
     }
@@ -114,9 +121,10 @@ int executeCommands(vector<Command>& commands)
     return 0;
 }
 
-
-void executeCommand(const Command& command)
+//Handles all the built-in commands. 
+void executeBuiltCommand(const Command& command)
 {
+    
     if (command.comm == "exit")
     {
         if(!command.args.empty())
@@ -153,15 +161,10 @@ void executeCommand(const Command& command)
         {
             currPaths.push_back(i);
         }
-        // for(auto i : currPaths)
-        // {
-        //     cout << "For execute: " << i << " ";
-        // }
-        // cout << endl;
-        
     }
 }
 
+//Check if the command is in the built-in command vector. 
 bool checkBuiltIn(const Command& command)
 {
     if(find(built_in.begin(), built_in.end(), command.comm) != built_in.end())
@@ -171,73 +174,24 @@ bool checkBuiltIn(const Command& command)
     return false;
 }
 
-int shell(Command& comm)
+
+/************************************************************
+*   Check if any one of the paths are valid executable files. 
+*   It return "-1" if none of the paths are valid.  
+*   Else it return the valid path string.
+*************************************************************/
+string isPathValid(const vector<string>& paths, const Command& comm)
 {
-    int filename = -1;
-    int savedStdOut = dup(1);
-    if(currPaths.empty())
-    {
-        char error_message[30] = "An error has occurred\n";
-        write(STDERR_FILENO, error_message, strlen(error_message));
-        return 0; 
-    }
-    if(find(comm.args.begin(), comm.args.end(), ">") != comm.args.end())
-    {
-        
-        int countOfOp = 0;
-        int idx = 0;
-        for(int i = 0; i<comm.args.size(); ++i)
-        {
-            if (comm.args[i] == ">")
-            {
-                countOfOp++;
-                idx = i;
-            }
-        }
-        //cout << comm.args[0] << endl;
-        if(countOfOp > 1 || idx != comm.args.size() - 2)
-        {
-            char error_message[30] = "An error has occurred\n";
-            write(STDERR_FILENO, error_message, strlen(error_message));
-            return 0; 
-        }
-        
-        Fclose(Fopen(comm.args.back().c_str(), "w"));
-        int file = Open(comm.args.back().c_str(), O_WRONLY | O_APPEND | O_CREAT, 0644);
-        filename = file;
-        comm.args.pop_back();
-        comm.args.pop_back();
-        Dup2(file, 1);
-        Close(file);
-    }
-    // if(comm.comm.size() > 3 && (comm.comm.substr(comm.comm.size() - 3) == ".sh"))
-    // {
-        
-    //     if (access(path.c_str(), X_OK) == -1)
-    //     {
-    //         char error_message[30] = "An error has occurred\n";
-    //         write(STDERR_FILENO, error_message, strlen(error_message));
-    //         return 0; 
-    //     }
-    //     string shellArgument = "sh";
-    //     string shellPath = path;
-    //     commPath = "/bin/sh";
-    //     args[0] = const_cast<char*>(shellArgument.c_str());
-    //     args[1] = const_cast<char*>(shellPath.c_str());
-    //     args[2] = NULL;
-    //     //cout << "hiii";
-    // }
-    
-    string path = currPaths.front() + "/" + comm.comm;
+    bool isValid =false;
+    string path = paths.front() + "/" + comm.comm;
     int i = 0;
     
     if (access(path.c_str(), X_OK) == -1)
     {
         
-        while (i < currPaths.size())
+        while (i < paths.size())
         {
-            //cout << currPaths[i] << endl;
-            path = currPaths[i] + "/" + comm.comm;
+            path = paths[i] + "/" + comm.comm;
             
             if(access(path.c_str(), X_OK) == 0)
             {
@@ -246,18 +200,29 @@ int shell(Command& comm)
             ++i;
         }
         
-        if (access(path.c_str(), X_OK) == -1)
-        {
-            char error_message[30] = "An error has occurred\n";
-            write(STDERR_FILENO, error_message, strlen(error_message));
-            return 0; 
-        }
+    }
+    if (access(path.c_str(), X_OK) == -1)
+    {
+        
+        isValid = false;
+    }
+    else
+    {
+        isValid = true;
+    }
+    if(isValid)
+    {
+        return path;
     }
     
+    return "-1";
     
-    string commPath = path;
-    char *args[256];
-    
+}
+
+
+int parseCommand(const Command& comm, char* args[], string& commPath, string& path)
+{
+    //Parsing command based on the number of arguments. This is not the final parsing that needs to be done!
     if (!comm.args.empty())
     {
         args[0] = const_cast<char*>(commPath.c_str());
@@ -273,69 +238,163 @@ int shell(Command& comm)
         args[0] = const_cast<char*>(commPath.c_str());
         args[1] = NULL;
     }
-    // string s = comm.comm.substr(comm.comm.size() - 3);
-    // //cout << (s == ".sh");
+    
+    
+    return 0;
+}
+
+
+/**********************************************************************************************************
+*   Check if the command includes a redirection option. 
+*   If redirect is 1 then there was an error with the command entered, if it was -1 then no redirect needed. 
+*   If 0 then redirection has been done.
+***********************************************************************************************************/
+int checkForRedirect(Command& comm)
+{
+    //Check if '>' exists in our argument list.
+    if(find(comm.args.begin(), comm.args.end(), ">") != comm.args.end())
+    {
+        
+        int countOfOp = 0;
+        int idx = 0;
+
+        //Count the number of '>' characters. Get the index of the last '>' character
+        for(int i = 0; i<comm.args.size(); ++i)
+        {
+            if (comm.args[i] == ">")
+            {
+                countOfOp++;
+                idx = i;
+            }
+        }
+
+        //If more than one '>' characters exist or '>' is not the second last argument element then it is an error. 
+        if(countOfOp > 1 || idx != comm.args.size() - 2)
+        {
+            char error_message[30] = "An error has occurred\n";
+            write(STDERR_FILENO, error_message, strlen(error_message));
+            return 1; 
+        }
+        
+        //Open file with O_TRUNC option to truncate the file. 
+        int file = Open(comm.args.back().c_str(), O_TRUNC|O_WRONLY|O_CREAT, 0600);
+        
+        //Closing the file after redirection as it is a redundant file descriptor after redirection. 
+        Dup2(file, 1);
+        Close(file);
+
+        //Removing the redirection argument as the direction has been done. 
+        comm.args.pop_back();
+        comm.args.pop_back();
+    }
+    else
+    {
+        return -1;
+    }
+    return 0;
+}
+
+int shell(Command& comm)
+{
+
+    //Save the original STDOUT
+    int savedStdOut = dup(1);
+
+    //Check if path is empty. 
+    if(currPaths.empty())
+    {
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        return 1; 
+    }
+
+    /*
+    *   Check if the command includes a redirection option. 
+    *   If redirect is 1 then there was an error with the command entered, if it was -1 then no redirect needed. 
+    *   If 0 then redirection has been done.
+    */
+    int redirect = checkForRedirect(comm);
+    if(redirect == 1)
+    {
+        return 1;
+    }
+    
+    
+    //Checking if the any one of the paths in current path array is valid.
+    string path;
+    if ((path = isPathValid(currPaths, comm)) == "-1")
+    {   
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        return 1;
+    }
+    
+    //Parse the command so that we can use it for execvp function. 
+    string commPath = comm.comm;
+    char *args[256];
+    if(parseCommand(comm, args, commPath, path) == 1)
+    {
+        return 1;
+    }
+
+    //Check if the command is a bash script. If it is then change args and commPath to match the bash script parameters for execvp. 
     if(comm.comm.size() > 3 && (comm.comm.substr(comm.comm.size() - 3) == ".sh"))
     {
-        //cout << path;
+        
         if (access(path.c_str(), X_OK) == -1)
         {
             char error_message[30] = "An error has occurred\n";
             write(STDERR_FILENO, error_message, strlen(error_message));
-            return 0; 
+            return 1; 
         }
+        
         string shellArgument = "sh";
         string shellPath = path;
         commPath = "/bin/sh";
-        args[0] = const_cast<char*>(shellArgument.c_str());
-        args[1] = const_cast<char*>(shellPath.c_str());
+        char *arg0 =  const_cast<char*>(shellArgument.c_str());
+        char *arg1 =  const_cast<char*>(shellPath.c_str());
+        args[0] = arg0;
+        args[1] = arg1;
         args[2] = NULL;
-        //cout << "hiii";
     }
-    //cout << currPaths.back();
     
-    // cout << "HIII";
-    // string s = "sh";
-    // string s1 = "tests/p1.sh";
-    // args[0] = const_cast<char*>(s.c_str());
-    // args[1] = const_cast<char*>(s1.c_str());
-    // args[2] = NULL;
-    
-    pid_t pid = Fork();
-    
+    //Forking a childing process. 
+    pid_t pid = Fork();  
     if(pid != 0)
     {
+        /****************************
+        * This is the parent process*
+        *****************************/
         
-        
+        //Waiting for the child process o finish. 
         Waitpid(pid, NULL, 0);
-        if(filename != -1)
+        
+        if(redirect != -1)
         {
-            //Close(filename);
+            //Restore the usual STDOUT if it was redirected. 
             Dup2(savedStdOut, 1);
         }
-        // if(file != "")
-        // {
-        //     Fclose(Fopen(file.c_str(), "w"));
-        // }
-        //printf("%d has exited \n", pid);
         
     }
     else
     {
-        
-        if ((execve(commPath.c_str(), args, NULL)) < 0)
+        /****************************
+        * This is the child process*
+        *****************************/
+
+        //Calling execvp. If it returns then command failed to execute.
+        if ((execvp(commPath.c_str(), args)) < 0)
         {
-            
             char error_message[30] = "An error has occurred\n";
             write(STDERR_FILENO, error_message, strlen(error_message)); 
         }
-        
-        exit(0);
+        exit(1);
     }
     
     return 0;
 }
 
+//Batch file to process files an inputs. 
 int batchFile(char* filename)
 {
     FILE* fp;
@@ -368,12 +427,8 @@ int batchFile(char* filename)
 
     
     // /* main loop */
-    
-    
     while(Fgets(linestr, 256, fp))
     {
-        
-        
         // make sure line has a '\n' at the end of it
         if(!strstr(linestr, "\n"))
             strcat(linestr, "\n");
@@ -383,14 +438,28 @@ int batchFile(char* filename)
         yy_switch_to_buffer(buffer);
         toks = gettoks();
         yy_delete_buffer(buffer);
-        if(toks[0] != NULL){
-            
+
+        if(toks[0] != NULL)
+        {
             string str = toks[0];
             comm.comm = str;
+
+            //Check if command is '&' or not.
+            if(comm.comm == "&")
+            {
+                comm = {};
+                continue;
+            }
+
             /* simple loop to echo all arguments */
             int ii = 1;
-            while(toks[ii] != NULL){
-                //printf( "Argument %d: %s\n", ii, toks[ii] );
+
+            while(toks[ii] != NULL)
+            {
+                /*
+                *   If we encounter '&' character then we can append the command to commands vector 
+                *   and start reading the line again for another comamnd.
+                */
                 if (strcmp(toks[ii], "&") == 0)
                 {
                     commands.push_back(comm);
@@ -404,29 +473,19 @@ int batchFile(char* filename)
                     {
                         break;
                     }
-                    
                 }
                 else
                 {
                     comm.args.push_back(toks[ii]);
                     ++ii;
                 }
-                
             }
             if(comm.comm != "")
             {
                 commands.push_back(comm);
             }
             
-            // for(auto j : commands)
-            // {
-            //     cout << "Command: " << j.comm << " ";
-            //     for(auto k : j.args)
-            //     {
-            //         cout << "args: " << k << " ";
-            //     }
-            //     cout << "\n";
-            // }
+            //Clearing commands and comm after executing the command(s) to start reading reading for another command. 
             executeCommands(commands);
             commands.clear();
             comm = {};
@@ -476,10 +535,9 @@ int main(int argc, char *argv[])
 
         
         /* main loop */
-        cout << "hfsh2> ";
+        cout << "hfsh> ";
         
         while(Fgets(linestr, 256, stdin)){
-            //currPaths.push_back("bin");
             // make sure line has a '\n' at the end of it
             if(!strstr(linestr, "\n"))
                 strcat(linestr, "\n");
@@ -494,10 +552,21 @@ int main(int argc, char *argv[])
                 
                 string str = toks[0];
                 comm.comm = str;
+
+                //Check if command is '&' or not. 
+                if(comm.comm == "&")
+                {
+                    comm = {};
+                    continue;
+                }
                 /* simple loop to echo all arguments */
                 int ii = 1;
                 while(toks[ii] != NULL){
-                    //printf( "Argument %d: %s\n", ii, toks[ii] );
+
+                    /*
+                    *   If we encounter '&' character then we can append the command to commands vector 
+                    *   and start reading the line again for another comamnd.
+                    */
                     if (strcmp(toks[ii], "&") == 0)
                     {
                         commands.push_back(comm);
@@ -511,34 +580,25 @@ int main(int argc, char *argv[])
                         {
                             break;
                         }
-                        
                     }
                     else
                     {
                         comm.args.push_back(toks[ii]);
                         ++ii;
                     }
-                    
                 }
                 if(comm.comm != "")
                 {
                     commands.push_back(comm);
                 }
-                // for(auto j : commands)
-                // {
-                //     cout << "Command: " << j.comm << " ";
-                //     for(auto k : j.args)
-                //     {
-                //         cout << "args: " << k << " ";
-                //     }
-                //     cout << "\n";
-                // }
+                
+                //Clearing commands and comm after executing the command(s) to start reading reading for another command. 
                 executeCommands(commands);
                 commands.clear();
                 comm = {};
                 if(strcmp(toks[0], "exit") != 0)
                 {
-                    cout << "hfsh2> ";
+                    cout << "hfsh> ";
                 }
                 if(!strcmp(toks[0], STR_EXIT))
                 break;
